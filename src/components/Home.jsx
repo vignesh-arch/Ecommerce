bbc3ee94abfe60d494da8278c2e3478cdf39b4ce
vignesh-arch <vigneshvelmurugan050@gmail.com>
2023-09-04 20:32:54 +0530

    Fixed Bugs in Cart when Out of Stock

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,12 @@
 
-import { useContext,useState,useEffect } from 'react';
+import { useContext,useRef } from 'react';
 import { DataContext } from '../context/DataContext'
 import Product from './Product';
 import '../assets/styles/Home.css'
 
 const Home = () => {
   const {filteredProduct}=useContext(DataContext);
-  // const [productList,setProductList]=useState([...filteredProduct].map((product)=>{
-  //   return <Product className="products" key={product.id} product={product}/>
-  // }));
-  // useEffect(()=>{
-  //   setProductList([...filteredProduct].map((product)=>{
-  //     return <Product key={product.id} product={product}/>
-  //   }))
-  // },[filteredProduct])
+
   const productList=[...filteredProduct].map((product)=>{
       return <Product className="products" key={product.id} product={product}/>
     });
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,38 +1,60 @@
-import { useContext, useState } from "react";
+import { useContext,useState,useEffect,useRef } from "react";
 import { DataContext } from "../context/DataContext";
 import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import '../assets/styles/Product.css';
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Divider from "@mui/joy/Divider";
 import DoneIcon from '@mui/icons-material/Done';
+import { NotificationsActiveTwoTone, NotificationsNoneTwoTone } from "@mui/icons-material";
 const Product = (props) => {
     const navigate=useNavigate();
+    let notifyStyle={};
     const {addToCart,cartItems}=useContext(DataContext);
     const product={...props.product};
     const [styles,setStyles]=useState({
-      color:"#444444",cursor:"pointer"
+      color:"#999999",cursor:"pointer",position:"relative",top:"3.2rem",right:"0.5rem"
     });
-    const [cartIcon,setCartIcon]=useState(cartItems.itemId.includes(product.id)?{icon:<DoneIcon/>,text:"Go to cart"}:{icon:<AddShoppingCartIcon/>,text:"Add to Cart"});
+    const [cartIcon,setCartIcon]=useState(
+      product.is_in_inventory===true?
+      cartItems.itemId.includes(product.id)?
+      {icon:<DoneIcon/>,text:"Go to cart"}:
+      {icon:<AddShoppingCartIcon/>,text:"Add to Cart"}:
+      {icon:<NotificationsNoneTwoTone/>,text:"Notify Me"});
+    if(cartIcon.text==="Notify Me"){
+      notifyStyle={backgroundColor:"#f84f31",color:"white"}
+    }
+    else if(cartIcon.text==="Will Notify Soon"){
+      notifyStyle={backgroundColor:"#23c552",color:"#333333"}
+    }
     const onCartAdd=(e)=>{
       console.log(cartItems);
       if([...cartItems.itemId].includes(product.id)){
         console.log("found")
         navigate('/cart');
       }
+      else if(cartIcon.text==="Notify Me"){
+        setCartIcon({icon:<NotificationsActiveTwoTone/>,text:"Will Notify Soon"})
+      }
       else{
-        addToCart(product,"add");
-        setCartIcon({icon:<DoneIcon/>,text:"Go to Cart"});
+        if(cartIcon.text!=="Will Notify Soon"){
+          addToCart(product,"add");
+          setCartIcon({icon:<DoneIcon/>,text:"Go to Cart"});
+        }
       }
     }
-    
+
     return (
     <div className="container">
         <div className="title-container">
           <p className="category">{product.category}</p>
-          <FavoriteTwoToneIcon onClick={()=>{setStyles(styles.color==="#444444"?{
-            color:"red",cursor:"pointer"
-          }:{color:"#444444",cursor:"pointer"})}} sx={styles}/>
+          <FavoriteTwoToneIcon 
+            onClick={()=>{setStyles(styles.color==="#999999"?
+              {...styles,color:"red",cursor:"pointer"}:
+              {...styles,color:"#999999",cursor:"pointer"}
+              )}
+            }
+            sx={styles}/>
         </div>
         <img src={product.imageURL} alt={product.name}/>
         <div className="price-container"> 
@@ -51,7 +73,7 @@ const Product = (props) => {
               <p className="price-title">Price</p>
               <p className="price">${product.price}</p>
             </div>
-            <button onClick={onCartAdd}>
+            <button style={notifyStyle} onClick={onCartAdd}>
               <div className="add-cart">
                 {cartIcon.icon}<p>{cartIcon.text}</p>
               </div>
